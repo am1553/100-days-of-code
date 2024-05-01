b5f6b94339b662aa4d06891251d670af989763b6
Aryan Mehta <aryanmehta2016@gmail.com>
2024-05-01 22:59:12 +0100

    completed calculator challenge

---
+++ beginners/calculator.py
@@ -0,0 +1,40 @@
+calculator_ascii = """
+ _____________________
+|  _________________  |
+| |              0. | |
+| |_________________| |
+|  ___ ___ ___   ___  |
+| | 7 | 8 | 9 | | + | |
+| |___|___|___| |___| |
+| | 4 | 5 | 6 | | - | |
+| |___|___|___| |___| |
+| | 1 | 2 | 3 | | x | |
+| |___|___|___| |___| |
+| | . | 0 | = | | / | |
+| |___|___|___| |___| |
+|_____________________|
+"""
+
+
+def calculate():
+    print(calculator_ascii)
+    first_number = int(input("What's the first number?: "))
+    operation = input("\n + \n - \n * \n / \n Pick an operation?: ")
+    second_number = int(input("What's the second number?: "))
+    result = 0
+    if operation == "+":
+        result = first_number + second_number
+    elif operation == "-":
+        result = first_number - second_number
+    elif operation == "*":
+        result = first_number * second_number
+    elif operation == "/":
+        result = first_number / second_number
+    else:
+        print("Invalid operation!")
+    print(f"{first_number} {operation} {second_number} = {result}")
+
+
+if __name__ == "__main__":
+    calculate()
+
